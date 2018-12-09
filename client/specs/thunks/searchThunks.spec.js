8c47f6ea342e568cb63a2a3de81d03240259e3a1
abdulfataiaka <abdulfataiaka@gmail.com>
2018-12-09 18:12:34 +0100

    feat(endpoint): implement search endpoint
    - add search endpoint
    - connect endpoint to frontend
    - write test for searchThunks
    - write test endpoint
    
    [Delivers #162531392]

--- client/specs/thunks/searchThunks.spec.js
+++ client/specs/thunks/searchThunks.spec.js
@@ -1,6 +1,10 @@
+import moxios from 'moxios';
 import mockStore from '../__mocks__/store';
-import { setSearchResult } from '../../thunks/searchThunks';
-import { SEARCH_HELP_FETCH_SUCCESS } from '../../utilities/actionTypes';
+import { setSearchResult, search } from '../../thunks/searchThunks';
+import {
+  SEARCH_HELP_FETCH_SUCCESS,
+  SEARCH_HELP_FETCH_FAILURE
+} from '../../utilities/actionTypes';
 
 describe('setSearchResult', () => {
   it('should return an array of expected actions', () => {
@@ -14,5 +18,66 @@ describe('setSearchResult', () => {
 
     store.dispatch(setSearchResult(helps));
     expect(store.getActions()).toEqual(expectedActions);
+  });
+});
+
+
+describe('search', () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  it('should return an array of expected actions for success request', (done) => {
+    const store = mockStore();
+    const helps = [];
+    const query = 'hello';
+
+    const expectedActions = [
+      {
+        type: SEARCH_HELP_FETCH_SUCCESS,
+        helps
+      }
+    ];
+
+    moxios.stubRequest('/api/search', {
+      status: 200,
+      response: {
+        helps
+      }
+    });
+
+    store.dispatch(search(query))
+      .then(() => {
+        expect(store.getActions()).toEqual(expectedActions);
+        done();
+      });
+  });
+
+  it('should return an array of expected actions for error request', (done) => {
+    const store = mockStore();
+    const query = 'hello';
+
+    const expectedActions = [
+      {
+        type: SEARCH_HELP_FETCH_FAILURE
+      }
+    ];
+
+    moxios.stubRequest('/api/search', {
+      status: 500,
+      response: {
+        error: {}
+      }
+    });
+
+    store.dispatch(search(query))
+      .then(() => {
+        expect(store.getActions()).toEqual(expectedActions);
+        done();
+      });
   });
 });
